6f45798d5e6df1f1c46ce2d57cd8f7f74dce159f
Abdullah Ramadan <74230257+Abdullahali310@users.noreply.github.com>
2023-02-06 15:58:23 +0200

    updated

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
-import { urlFor } from '../lib/client';
 
+import { urlFor } from '../lib/client';
 const Product = ({ product: { image, name, slug, price } }) => {
+  console.log(slug?.current);
   return (
     <div>
-      <Link href={`/Product/${slug?.current.toString()}`}>
-
-        <div className="product-card">
-          <img
+      <Link href={`/product/${slug?.current ?? ''}`}>
+        <a className="product-card">
+          <img 
             src={urlFor(image && image[0])}
             width={250}
             height={250}
             className="product-image"
+            alt={name}
           />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
-        </div>
+        </a>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+
+export default Product;
